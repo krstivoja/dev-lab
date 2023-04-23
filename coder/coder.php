a1ad1281051461b97820a3a2af6fb1ec3f62a678
Marko Krstic <mailme@markokrstic.com>
2023-04-23 15:06:26 +0200

    ...

--- coder/coder.php
+++ coder/coder.php
@@ -29,3 +29,20 @@ function coder_enqueue_admin_css() {
     wp_enqueue_script('coder-admin-js', plugins_url('js/coder-script.js', __FILE__), array('jquery'), '1.0.0', true);
 }
 add_action('admin_enqueue_scripts', 'coder_enqueue_admin_css');
+
+
+
+// AJAX action for saving the file content
+function coder_save_file_content() {
+    $file = sanitize_text_field($_POST['file']);
+    $content = $_POST['content'];
+    $dir = wp_upload_dir()['basedir'].'/code/';
+
+    if (is_dir($dir) && file_exists($dir.'/'.$file)) {
+        file_put_contents($dir.'/'.$file, $content);
+        wp_send_json_success('File saved successfully.');
+    } else {
+        wp_send_json_error('Oops! Could not save the file.');
+    }
+}
+add_action('wp_ajax_coder_save_file_content', 'coder_save_file_content');
