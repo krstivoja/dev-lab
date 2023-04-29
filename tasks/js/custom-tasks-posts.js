2b916e5ec4ee204d485a5832be3645a96b8bd0fe
Marko Krstic <mailme@markokrstic.com>
2023-04-29 12:13:30 +0200

    ...

--- tasks/js/custom-tasks-posts.js
+++ tasks/js/custom-tasks-posts.js
@@ -1,29 +1,28 @@
-jQuery(document).ready(function($) {
-    $('.tasks-list').each(function() {
+jQuery(document).ready(function ($) {
+  $('.tasks-list').each(function () {
       new Sortable(this, {
-        group: 'tasks',
-        onEnd: function(event) {
-          var item = event.item;
-  
-          // Get the post ID and the new term slug
-          var post_id = $(item).data('post_id');
-          var new_term_slug = $(item).parent('.tasks-list').siblings('.tasks-group-title').data('term_slug');
-  
-          // Send AJAX request to update the post term
-          $.ajax({
-            url: ajaxurl,
-            type: 'POST',
-            data: {
-              action: 'update_post_term',
-              post_id: post_id,
-              term_slug: new_term_slug,
-            },
-            success: function(response) {
-              console.log(response);
-            }
-          });
-        }
+          group: 'tasks',
+          onEnd: function (event) {
+              var item = event.item;
+
+              // Get the post ID and the new term slug
+              var post_id = $(item).data('post_id');
+              var new_term_slug = $(item).parent('.tasks-list').siblings('.tasks-group-title').data('term_slug');
+
+              // Send AJAX request to update the post term
+              $.ajax({
+                  url: custom_tasks_posts_data.ajaxurl,
+                  type: 'POST',
+                  data: {
+                      action: 'update_post_term',
+                      post_id: post_id,
+                      term_slug: new_term_slug,
+                  },
+                  success: function (response) {
+                      console.log(response);
+                  }
+              });
+          }
       });
-    });
   });
-  
+});
--- tasks/tasks.php
+++ tasks/tasks.php
@@ -9,74 +9,89 @@
  * License: GPL2
  */
 
- function custom_tasks_posts_shortcode() {
+add_shortcode("custom_tasks_posts", "custom_tasks_posts_shortcode");
+function custom_tasks_posts_shortcode()
+{
     ob_start();
-  
-    $terms = get_terms(array(
-      'taxonomy' => 'progress',
-      'hide_empty' => false,
-    ));
-  
+
+    $terms = get_terms([
+        "taxonomy" => "progress",
+        "hide_empty" => false,
+    ]);
+
     if ($terms) {
-      foreach ($terms as $term) {
-        $args = array(
-          'post_type' => 'tasks',
-          'posts_per_page' => -1,
-          'tax_query' => array(
-            array(
-              'taxonomy' => 'progress',
-              'field' => 'slug',
-              'terms' => $term->slug,
-            ),
-          ),
-          'orderby' => 'menu_order',
-          'order' => 'ASC',
-        );
-  
-        $query = new WP_Query($args);
-  
-        if ($query->have_posts()) {
-          echo "<div class='tasks-group' data-term_slug='$term->slug'>";
-  
-          echo "<h2 class='tasks-group-title'>$term->name</h2>";
-  
-          echo "<div class='tasks-list'>";
-  
-          while ($query->have_posts()) {
-            $query->the_post();
-  
-            echo "<div class='task-item' data-post_id='" . get_the_ID() . "'>";
-            the_title();
-            echo "</div>";
-          }
-  
-          echo "</div>"; // End of .tasks-list
-  
-          echo "</div>"; // End of .tasks-group
+        foreach ($terms as $term) {
+            $args = [
+                "post_type" => "tasks",
+                "posts_per_page" => -1,
+                "tax_query" => [
+                    [
+                        "taxonomy" => "progress",
+                        "field" => "slug",
+                        "terms" => $term->slug,
+                    ],
+                ],
+                "orderby" => "menu_order",
+                "order" => "ASC",
+            ];
+
+            $query = new WP_Query($args);
+
+            if ($query->have_posts()) {
+                echo "<div class='tasks-group' data-term_slug='$term->slug'>";
+
+                echo "<h2 class='tasks-group-title'>$term->name</h2>";
+
+                echo "<div class='tasks-list'>";
+
+                while ($query->have_posts()) {
+                    $query->the_post();
+
+                    echo "<div class='task-item' data-post_id='" .
+                        get_the_ID() .
+                        "'>";
+                    the_title();
+                    echo "</div>";
+                }
+
+                echo "</div>"; // End of .tasks-list
+
+                echo "</div>"; // End of .tasks-group
+            }
         }
-      }
     }
-  
+
     wp_reset_postdata();
     return ob_get_clean();
-  }
-  
-  function custom_tasks_posts_enqueue_scripts() {
-    wp_enqueue_script('sortablejs', 'https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js', array(), '', true);
-    
-    wp_enqueue_script('custom-tasks-posts', plugins_url('/js/custom-tasks-posts.js', __FILE__), array('jquery', 'sortablejs'), '', true);
-  }
+}
 
+function custom_tasks_posts_enqueue_scripts()
+{
+    wp_enqueue_script(
+        "sortablejs",
+        "https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js",
+        [],
+        "",
+        true
+    );
 
-  add_action('wp_enqueue_scripts', 'custom_tasks_posts_enqueue_scripts');
-  add_shortcode('custom_tasks_posts', 'custom_tasks_posts_shortcode');
-  
+    wp_enqueue_script(
+        "custom-tasks-posts",
+        plugins_url("/js/custom-tasks-posts.js", __FILE__),
+        ["jquery", "sortablejs"],
+        "",
+        true
+    );
+}
+
+add_action("wp_enqueue_scripts", "custom_tasks_posts_enqueue_scripts");
 
 // Usage [custom_tasks_posts]
 
-function custom_tasks_posts_localize_script() {
-  wp_localize_script('custom-tasks-posts', 'custom_tasks_posts_data', array(
-    'ajaxurl' => admin_url('admin-ajax.php')
-  ));
+function custom_tasks_posts_localize_script()
+{
+    wp_localize_script("custom-tasks-posts", "custom_tasks_posts_data", [
+        "ajaxurl" => admin_url("admin-ajax.php"),
+    ]);
 }
-add_action('wp_enqueue_scripts', 'custom_tasks_posts_localize_script');
+add_action("wp_enqueue_scripts", "custom_tasks_posts_localize_script");
