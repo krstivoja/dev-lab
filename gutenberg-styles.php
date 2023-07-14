1af9a90aaca0a08d950c506b5ab0be810f4e1928
Marko Krstic <mailme@markokrstic.com>
2023-07-14 19:00:03 +0200

    ..

--- gutenberg-styles.php
+++ gutenberg-styles.php
@@ -26,39 +26,47 @@ function render_gutenberg_styles_admin_page() {
   <aside>
       <input type="search" name="" id="">
       <div id="filter">
-          <div class="tag">reusable</div>
-          <div class="tag">text</div>
-          <div class="tag">layout</div>
-          <div class="tag">media</div>
-          <div class="tag">theme</div>
-          <div class="tag">embed</div>
-          <div class="tag">design</div>
-          <div class="tag">widgets</div>
+        <?php
+        $block_categories = get_categories(array('taxonomy' => 'block_directory'));
+        foreach ($block_categories as $category) {
+          echo '<div class="tag">' . esc_html($category->name) . '</div>';
+        }
+        ?>
       </div>
 
       <div id="blocks">
-          <div id="reusable">
-              <h2 class="group-title">design</h2>
+          <div id="text-blocks" class="blocks-group">
+              <h2 class="group-title">Text Blocks</h2>
               <div class="blocks-list">
-                  <div class="block">Buttons</div>
-                  <div class="block">Column</div>
-                  <div class="block">Columns</div>
-                  <div class="block">Group</div>
-                  <div class="block">More</div>
-                  <div class="block">Page Break</div>
-                  <div class="block">Separator</div>
-                  <div class="block">Spacer</div>
-                  <div class="block">Text Columns (deprecated)</div>
-                  <div class="block">Comment Template</div>
-                  <div class="block">Home Link</div>
-                  <div class="block">Custom Link</div>
-                  <div class="block">Submenu</div>
+                <div class="block">Paragraph</div>
+                <div class="block">Heading</div>
+                <div class="block">List</div>
+                <div class="block">Quote</div>
+                <div class="block">Classic</div>
+                <div class="block">Code</div>
+                <div class="block">Preformatted</div>
+                <div class="block">Pullquote</div>
+                <div class="block">Table</div>
+                <div class="block">Verse</div>
+              </div>
+          </div>
+
+          <div id="media-blocks" class="blocks-group">
+              <h2 class="group-title">Media Blocks</h2>
+              <div class="blocks-list">
+                  <div class="block">Image</div>
+                  <div class="block">Gallery</div>
+                  <div class="block">Audio</div>
+                  <div class="block">Cover</div>
+                  <div class="block">File</div>
+                  <div class="block">Media & Text</div>
+                  <div class="block">Video</div>
               </div>
           </div>
       </div>
   </aside>
   <main>
-      <div class="block-title">Add dinamicaly block title</div>
+      <div class="block-title">Paragraph</div>
   </main>
 </div> 
 
